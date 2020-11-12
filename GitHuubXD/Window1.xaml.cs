4c0e6e2ab6e9ea6fe695515c9ca2da363906cef4
Grzegorz Kielian <grzegorzkielian99@gmail.com>
2020-11-12 10:41:50 +0100

    Lista - Odczyt/Edit

---
+++ GitHuubXD/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GitHuubXD
+{
+
+    public partial class Window1 : Window
+    {
+        private string namefield;
+        private string surnamefield;
+        private string peselfield;
+        private string cityfield;
+        private string adressfield;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            namefield = Name.Text;
+            surnamefield = Surname.Text;
+            peselfield = Pesel.Text;
+            cityfield = City.Text;
+            adressfield = Adress.Text;
+            try
+            {
+                MainWindow.PersonList.Add(new MainWindow.Person() { Name = namefield, Surname = surnamefield, Pesel = peselfield, City = cityfield, Adress = adressfield });
+            }
+            catch(Exception blad)
+            {
+                MessageBox.Show(blad.Message);
+            }
+        }
+    }
+}
---
+++ GitHuubXD/Window2.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GitHuubXD
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+
+        public Window2(int item)
+        {
+            int klucz = item;
+            InitializeComponent();
+            Name.Text = MainWindow.PersonList[klucz].Name;
+            Surname.Text = MainWindow.PersonList[klucz].Surname;
+            Pesel.Text = MainWindow.PersonList[klucz].Pesel;
+            City.Text = MainWindow.PersonList[klucz].City;
+            Adress.Text = MainWindow.PersonList[klucz].Adress;
+            x.Content = klucz;
+        }
+
+        private void Modify(object sender, RoutedEventArgs e)
+        {
+            MainWindow.PersonList[Convert.ToInt32(x.Content)].Name = Name.Text;
+            MainWindow.PersonList[Convert.ToInt32(x.Content)].Surname = Surname.Text;
+            MainWindow.PersonList[Convert.ToInt32(x.Content)].Pesel = Pesel.Text;
+            MainWindow.PersonList[Convert.ToInt32(x.Content)].City = City.Text;
+            MainWindow.PersonList[Convert.ToInt32(x.Content)].Adress = Adress.Text;
+            this.Close();
+        }
+    }
+}
